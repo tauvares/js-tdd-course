ead12c62c5b273cc28081408a3905d8228b2be30
João Tavares Filho Segundo <tauvares@gmail.com>
2018-05-06 03:40:27 -0300

    Add husky

---
+++ src/main.js
@@ -0,0 +1,9 @@
+const a = 3;
+const b = 2;
+
+const c = 'udsgdsgfdsgfdsgfdsfdgsoupyhersu nser hruhtueshtiuseirthiusonuirnhoiusnhiumreuithmsuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu';
+
+function sum(){
+  return a + b + c;
+}
+sum();
